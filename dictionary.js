fd8b255896e4b48be858b45dde4fed6c2d2bbd0b
Eduardo Vergara <eduver_san@hotmail.com>
2019-09-19 06:08:24 -0500

    First commit

---
+++ dictionary.js
@@ -0,0 +1,110 @@
+let diccionary = [
+    {
+        english: "FOR REISSUE/REVALIDATION",
+        spanish: "por reexpedición"
+    },
+    {
+        english: "FOR REISSUE/\nREVALIDATION",
+        spanish: "por reexpedición"
+    },
+    {
+        english: "CHILD/INFANT DISCOUNTS APPLY",
+        spanish: "**"
+    },
+    {
+        english: "NOTE - TEXT BELOW NOT VALIDATED FOR AUTOPRICING.",
+        spanish: "**"
+    },
+    {
+        english: "WAIVED FOR SCHEDULE CHANGE/ILLNESS OR DEATH OF",
+        spanish: "**"
+    },
+    {
+        english: "PASSENGER OR FAMILY MEMBER",
+        spanish: "**"
+    },
+    {
+        english: "WAIVER APPLIES FOR THE FAMILY MEMBER FIRST DEGREE",
+        spanish: "**"
+    },
+    {
+        english: "AND/OR FOR TRAVELING COMPANION",
+        spanish: "**"
+    },
+    {
+        english: "CANCELLATIONS PERMITTED",
+        spanish: "Cancelaciones permitidas"
+    },
+    {
+        english: "CHANGES PERMITTED",
+        spanish: "Cambios permitidos"
+    },
+    {
+        english: "TICKET IS NON-REFUNDABLE",
+        spanish: "Tiquete no reembolsable"
+    },
+    {
+        english: "IN CASE OF",
+        spanish: "en caso de "
+    },
+    {
+        english: "CHANGE IS NOT PERMITTED",
+        spanish: "cambios no permitidos"
+    },
+    {
+        english: "DAYS BEFORE DEPARTURE",
+        spanish: "días antes del viaje"
+    },
+    {
+        english: "IF CHANGES ARE MADE WITHIN 30 DAYS BEFORE",
+        spanish: "Si el cambio es realizado 30 días antes"
+    },
+    {
+        english: "SHOULD BE APPLIED",
+        spanish: "será aplicado"
+    },
+    {
+        english: "DEPARTURE",
+        spanish: "del viaje"
+    },
+    {
+        english: "A FEE OF",
+        spanish: "un cargo de"
+    },
+    {
+        english: "CHANGE PERMITTED WITH",
+        spanish: "cambio permitido"
+    },
+    {
+        english: "FREE OF CHARGE",
+        spanish: "sin cargo"
+    },
+    {
+        english: "UP TO",
+        spanish: "hasta"
+    },
+    {
+        english: "CHARGE",
+        spanish: "Cargo"
+    },
+    {
+        english: "FOR REFUND",
+        spanish: "para reembolso"
+    },
+    {
+        english: "ANY TIME",
+        spanish: "En cualquier momento"
+    }
+];
+
+let titles = [
+    "ORIGIN",
+    "CANCELLATIONS\n", 
+    "CHANGES\n",
+    "\n1ST",
+    "\nIF CHANGE"
+];
+
+let subtitles = [
+    "ANY TIME"
+]
